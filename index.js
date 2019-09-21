d0dc5af169c002abb21d77ea8c6c83c3419890c2
Heidi B <boulter.heidi@gmail.com>
2019-09-21 19:48:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ function sayHiToGrandma(string) {
   if (lowercase.toLowerCase() === lowercase) {
     return "I can't hear you!"
   }
-  
+
   else if (uppercase.toUpperCase() === uppercase) {
     return "YES INDEED!"
   }
-
